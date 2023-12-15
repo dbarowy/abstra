a5dc6a16cbb7397c810e1232ef6472a6b2d69245
Sammy-Sasaki <124902186+Sammy-Sasaki@users.noreply.github.com>
2023-12-14 22:52:51 -0800

    added some tests

--- codeTests/Tests.fs
+++ codeTests/Tests.fs
@@ -4,9 +4,34 @@ open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
 open AST
 open Shapes
+open Parser
+open Evaluator
 
 [<TestClass>]
 type TestClass () =
+
+    [<TestMethod>]
+    member this.TestParser () =
+        let text = "4 shapes, scheme: red blue purple green, max 3 edges"
+        match parse text with
+        | Some ast ->
+            let svg = ast
+            let test = Expression(4, Colors [Red; Blue; Purple; Green], 3)
+            Assert.AreEqual(svg, test)
+        | None ->
+            printfn "Invalid program."
+            Assert.Fail()
+
+    [<TestMethod>]
+    member this.testInterpreter () =
+        let expr = Expression(4, Colors [Red; Blue; Purple; Green], 3)
+        let svg = eval expr
+        ()
+
+    [<TestMethod>]
+    member this.TestPointGenerator() =
+        let pts = generatePoints 6 6
+        Assert.AreEqual(6, pts.Length)
 
     [<TestMethod>]
     member this.TestInIntersects () =
